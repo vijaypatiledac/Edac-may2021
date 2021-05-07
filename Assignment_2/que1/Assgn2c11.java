64bd79c36b6a01f560516e0642610fa6bf9caa43
vijaypatiledac <83824196+vijaypatiledac@users.noreply.github.com>
2021-05-07 22:30:36 +0530

    Add files via upload

---
+++ Assignment_2/que1/Assgn2c11.java
@@ -0,0 +1,19 @@
+class Assgn2c11
+{
+    public static void main(String args[])
+     {
+      for(int i=1;i<=5;i++)
+	{
+	   for(int j=4;j>=i;j--)
+	      {
+		System.out.print(" ");
+	      }
+	   for(int j=1;j<=(i*2)-1;j++)
+	      {
+		System.out.print("*");
+	      }
+	   System.out.println(" ");
+	}
+     
+     }
+}
---
+++ Assignment_2/que1/Assgn2c3.java
@@ -0,0 +1,19 @@
+class Assgn2c3
+{
+    public static void main(String args[])
+		{
+		
+		for(int i=1;i<=5;i++)
+	{    
+			
+			for(int j=1;j<=i;j++)
+			{
+			    char ch='A';
+				System.out.print(" *");
+				
+			}
+		System.out.println(" ");
+	}
+     
+		}
+}
