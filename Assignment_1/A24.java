e629f67a4437ae5b0113ae9b7028c37033a37e23
vijaypatiledac <83824196+vijaypatiledac@users.noreply.github.com>
2021-05-08 09:25:21 +0530

    Add files via upload

---
+++ Assignment_1/A24.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class A24
+{
+public static void main(String args[])
+{
+int oct[]= new int[100];
+int i=1, j=0, rem, dec=0, bin;
+Scanner sc= new Scanner(System.in);
+System.out.print("Binary No:");
+bin = sc.nextInt();
+while(bin>0)
+{
+rem = bin % 10;
+dec = dec + rem * i;
+i = i*2;
+bin = bin / 10;
+ }
+i = 0;
+while(dec != 0)
+{
+oct[i] = dec % 8;
+dec = dec / 8;
+i++;
+}
+System.out.print("Octal Value :");
+for(j = i-1; j >= 0; j--)
+{
+if(oct[j] > 9)
+{
+System.out.print((char)(oct[j] + 55)+"\n");
+}
+else
+{
+System.out.print(oct[j]+"\n");
+}
+}
+}
+}
